448900e0a38721336ff7b5ca784fc7a26d7faed6
JeongEun <lisasje25@gmail.com>
2021-04-15 16:34:07 +0900

    0415

---
+++ Algorithm_study/src/Map/MapTest.java
@@ -0,0 +1,35 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MapTest {
+
+	public static void main(String[] args) {
+		Map<Integer,String> map = new HashMap<>();
+		map.put(1, "A");
+		map.put(2, "B");
+		map.put(3, "C");
+		
+		//1.keySet
+		for(Integer key : map.keySet()) {
+			System.out.println("key:" + key + "values:" + map.get(key));
+		}
+		
+		System.out.println();
+		
+		//2.entrySet
+		for(Map.Entry<Integer, String> elem : map.entrySet()) {
+			System.out.println("key:" + elem.getKey() + "value:" + elem.getValue());
+		}
+		
+		//3.iterator
+		Iterator<Integer> keys = map.keySet().iterator();
+		while(keys.hasNext()) {
+			Integer key = keys.next();
+			System.out.println("key:" + key + "values:" + map.get(key));
+		}
+	}
+
+}
