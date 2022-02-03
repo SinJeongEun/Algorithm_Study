2c9d5ec5b4e706c83c3badb6906550cf1138c9b5
JeongEun <lisasje25@gmail.com>
2022-02-04 02:12:19 +0900

    0203

--- Algorithm_study/src/heap/DiskController.java
+++ Algorithm_study/src/heap/DiskController.java
@@ -4,9 +4,18 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class DiskController {
+	class Job{
+		int start;
+		int time;
+
+		public Job(int start,int time) {
+			this.start = start;
+			this.time = time;
+		}
+	}
 
 		public int solution(int[][] jobs) {
-			PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]); //처리시간 순서대로
+			PriorityQueue<Job> q = new PriorityQueue<>((a, b) -> a.time - b.time); //처리시간 순서대로
 			Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]); //시작시간 순서대로
 
 			int answer = 0;
@@ -14,17 +23,16 @@ public class DiskController {
 			int i = 0;
 			while (i < jobs.length || !q.isEmpty()) {
 				while (i < jobs.length && jobs[i][0] <= current) {
-					q.add(new int[]{jobs[i][0], jobs[i][1]});
+					q.add(new Job(jobs[i][0],jobs[i][1]));
 					i++;
 				}
 
 				if (q.isEmpty()) { //큐가 비어있다. -> 현재 실행할 수 있는 작업이 없다.
 					current = jobs[i][0]; //다음 작업의 시작시간으로 이동한다.
 				} else {
-					int[] temp = q.poll();
-					answer += current + temp[1] - temp[0]; //요청 ~ 종료시간
-					System.out.println(current + " " + temp[0] + " " + temp[1]);
-					current += temp[1];
+					Job temp = q.poll();
+					answer += current + temp.time - temp.start; //요청 ~ 종료시간
+					current += temp.time;
 				}
 			}
 			return answer / jobs.length;
